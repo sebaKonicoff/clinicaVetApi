bb9a39b3eacee3cd59722e324ecebc4738cd71e1
SebaKonicoff <sebakonicoff@gmail.com>
2024-11-25 21:54:57 -0300

    Initial commit

---
+++ src/main/java/com/clinica_veterinaria/cliniccaVetApi/CliniccaVetApiApplication.java
@@ -0,0 +1,13 @@
+package com.clinica_veterinaria.cliniccaVetApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CliniccaVetApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CliniccaVetApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clinica_veterinaria/cliniccaVetApi/CliniccaVetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinica_veterinaria.cliniccaVetApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CliniccaVetApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
